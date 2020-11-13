30c4da85b42f512b83bcc7b740537fd46b1edc91
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-11-13 16:27:02 +0000

    Add typescript no-shadow

--- index.js
+++ index.js
@@ -191,6 +191,7 @@ module.exports = {
         "@typescript-eslint/restrict-plus-operands": "error",
         "@typescript-eslint/promise-function-async": "error",
         "@typescript-eslint/prefer-string-starts-ends-with": "error",
+        "@typescript-eslint/no-shadow",
         "@typescript-eslint/switch-exhaustiveness-check": "error",
         "@typescript-eslint/no-require-imports": "error",
         "@typescript-eslint/prefer-nullish-coalescing": "error",
